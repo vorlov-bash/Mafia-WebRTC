2c404b25c82a29d5f4f2dc89b08a1ba76f3ce49a
volodkaorlov <volodka.orlov@gmail.com>
2020-06-03 17:46:50 +0300

    Added proper camera positions.

--- src/controllers/room.ts
+++ src/controllers/room.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 
 export const index = (req: Request, res: Response) => {
-    res.render("room/client", {"roomName": req.params.roomName});
+    res.render("room/index", {"roomName": req.params.roomName, "title": `Room: ${req.params.roomName}`});
 };
 
 // export const createRoom = (req: Request, res: Response) => {
